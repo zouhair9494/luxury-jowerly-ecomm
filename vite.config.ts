d44fb03cfef7a153b64903bab92f3d97904f1b6d
Zouhair <zouhairhennane@gmail.com>
2026-06-01 16:43:41 +0200

    fix: base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
